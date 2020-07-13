afecbc3c5ee69c7abd4631040c4065be4fb36948
Mallory <ozuna.mallory@gmail.com>
2020-07-12 22:20:34 -0500

    Update Cart Service

--- src/main/java/com/tts/eCommerce/service/CartService.java
+++ src/main/java/com/tts/eCommerce/service/CartService.java
@@ -2,7 +2,19 @@ package com.tts.eCommerce.service;
 
 import org.springframework.stereotype.Service;
 
+import com.tts.eCommerce.model.Cart;
+
 @Service
 public class CartService {
+
+	public Cart updateCartItemQuantity(Cart cart, Long productId, Integer quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Cart addCartItemToCart(Cart cart, long productId, Integer quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
