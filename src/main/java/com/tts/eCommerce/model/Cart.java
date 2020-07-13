b1892bab15c1f1924ddcd5b3c10768272287e34d
Mallory <ozuna.mallory@gmail.com>
2020-07-12 21:17:47 -0500

    Update CartModel

--- src/main/java/com/tts/eCommerce/model/Cart.java
+++ src/main/java/com/tts/eCommerce/model/Cart.java
@@ -20,7 +20,55 @@ public class Cart {
 	public Double subTotal;
 	public Double total;
 	public List<HashMap<Product, Integer>> cartItems;
+	
 
 	public Cart() {
 	}
+
+	public Cart(Double subTotal, Double total, List<HashMap<Product, Integer>> cartItems) {
+		this.subTotal = subTotal;
+		this.total = total;
+		this.cartItems = cartItems;
+		
+	}
+
+	public Double getSubTotal() {
+		return subTotal;
+	}
+
+
+	public void setSubTotal(Double subTotal) {
+		this.subTotal = subTotal;
+	}
+
+
+	public Double getTotal() {
+		return total;
+	}
+
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+
+	public List<HashMap<Product, Integer>> getCartItems() {
+		return cartItems;
+	}
+
+
+	public void setCartItems(List<HashMap<Product, Integer>> cartItems) {
+		this.cartItems = cartItems;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [id=" + id + ", subTotal=" + subTotal + ", total=" + total + ", cartItems=" + cartItems + "]";
+	}
+	
 }
