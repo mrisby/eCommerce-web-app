a34511ce4ee87891f3b9f7e8fb88ae0cbfc316e8
Mallory <ozuna.mallory@gmail.com>
2020-07-12 22:20:07 -0500

    Update PurchaseOrder Repository

--- src/main/java/com/tts/eCommerce/repository/PurchaseOrderRepository.java
+++ src/main/java/com/tts/eCommerce/repository/PurchaseOrderRepository.java
@@ -1,5 +1,12 @@
 package com.tts.eCommerce.repository;
 
-public interface PurchaseOrderRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import com.tts.eCommerce.model.PurchaseOrder;
+
+@Repository
+public interface PurchaseOrderRepository extends CrudRepository<PurchaseOrder, Long>{
+
+	public PurchaseOrder findByPurchaseOrder(String purchaseOrder); 
 }
