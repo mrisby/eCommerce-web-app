ccca772901550be8ba325d5b2b1ebd93b20525f4
Mrisby <michellenrisby@gmail.com>
2020-07-12 19:10:36 -0500

    Update UserRepository to use JPA

--- src/main/java/com/tts/eCommerce/repository/UserRepository.java
+++ src/main/java/com/tts/eCommerce/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.tts.eCommerce.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tts.eCommerce.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	public User findByUsername(String username);
 
 	public void save(org.springframework.boot.autoconfigure.security.SecurityProperties.User user);
